f021de5519b20abdaf705ccdf92886fed38b0c92
SakodaShintaro <rgbygscrsedppbwg@gmail.com>
2018-10-26 18:07:17 +0900

    修正途中.これは地獄の臭いがするぞ……

--- kaitei_othello/move.hpp
+++ kaitei_othello/move.hpp
@@ -7,67 +7,24 @@
 #include<unordered_map>
 #include<iostream>
 
-enum MoveConst {
-	//0000 0000 0000 0000 0000 0000 0111 1111 to
-	//0000 0000 0000 0000 0011 1111 1000 0000 from
-	//0000 0000 0000 0000 0100 0000 0000 0000 drop
-	//0000 0000 0000 0000 1000 0000 0000 0000 promote
-	//0000 0000 1111 1111 0000 0000 0000 0000 subject
-	//1111 1111 0000 0000 0000 0000 0000 0000 capture
-	MOVE_TO_SHIFT = 0,
-	MOVE_FROM_SHIFT = 7,
-	MOVE_DROP_SHIFT = 14,
-	MOVE_PROMOTE_SHIFT = 15,
-	MOVE_SUBJECT_SHIFT = 16,
-	MOVE_CAPTURE_SHIFT = 24,
-	MOVE_TO_MASK = 0b1111111,
-	MOVE_FROM_MASK = MOVE_TO_MASK << MOVE_FROM_SHIFT,
-	MOVE_DROP_MASK = 1 << MOVE_DROP_SHIFT,
-	MOVE_PROMOTE_MASK = 1 << MOVE_PROMOTE_SHIFT,
-	MOVE_SUBJECT_MASK = 0xff << MOVE_SUBJECT_SHIFT,
-	MOVE_CAPTURE_MASK = 0xff << MOVE_CAPTURE_SHIFT,
-};
-
 class Move {
 public:
 	//コンストラクタ
 	//Move() : move(0), score(Score(0)) {}
     Move() = default;
-    Move(int x) : move(x), score(Score(x)) {}
-	Move(Square to, Square from) : move(from << MOVE_FROM_SHIFT | to << MOVE_TO_SHIFT), score(Score(0)) {}
-	Move(Square to, Square from, bool isDrop) : move(isDrop << MOVE_DROP_SHIFT | from << MOVE_FROM_SHIFT | to << MOVE_TO_SHIFT) {}
-	Move(Square to, Square from, bool isDrop, bool isPromote) : move(isPromote << MOVE_PROMOTE_SHIFT | isDrop << MOVE_DROP_SHIFT | from << MOVE_FROM_SHIFT | to << MOVE_TO_SHIFT) {}
-	Move(Square to, Square from, bool isDrop, bool isPromote, Piece subject) : move(subject << MOVE_SUBJECT_SHIFT | isPromote << MOVE_PROMOTE_SHIFT | isDrop << MOVE_DROP_SHIFT | from << MOVE_FROM_SHIFT | to << MOVE_TO_SHIFT) {}
-	Move(Square to, Square from, bool isDrop, bool isPromote, Piece subject, Piece capture) : move(capture << MOVE_CAPTURE_SHIFT | subject << MOVE_SUBJECT_SHIFT | isPromote << MOVE_PROMOTE_SHIFT | isDrop << MOVE_DROP_SHIFT | from << MOVE_FROM_SHIFT | to << MOVE_TO_SHIFT) {}
+    Move(int32_t x) : move(x), score(Score(x)) {}
 
 	//表示
 	void print() const {
-		std::cout << SquareToFile[to()] << SquareToRank[to()];
-		std::cout << PieceToStr[subject()];
-		if (isPromote()) fprintf(stdout, "成");
-		else if (isDrop()) fprintf(stdout, "打");
-		std::cout << "(" << SquareToFile[from()] << SquareToRank[from()] << ") ";
-		if (capture() != EMPTY) std::cout << "capture:" << PieceToStr[capture()] << std::endl;
-		else std::cout << std::endl;
+        std::cout << to() << std::endl;
 	}
 
 	void printWithScore() const {
-        std::cout << SquareToFile[to()] << SquareToRank[to()];
-        std::cout << PieceToStr[subject()];
-        if (isPromote()) fprintf(stdout, "成");
-        else if (isDrop()) fprintf(stdout, "打");
-        std::cout << "(" << SquareToFile[from()] << SquareToRank[from()] << ") ";
-        if (capture() != EMPTY) std::cout << "capture:" << PieceToStr[capture()];
-        std::cout << "\tscore:" << score << std::endl;
+        std::cout << to() << "\tscore:" << score << std::endl;
 	}
 	
 	//要素を取り出す関数ら
-	inline Square to() const { return static_cast<Square>(move & MOVE_TO_MASK); }
-	inline Square from() const { return static_cast<Square>((move & MOVE_FROM_MASK) >> MOVE_FROM_SHIFT); }
-	inline bool isDrop() const { return (move & MOVE_DROP_MASK) != 0; }
-	inline bool isPromote() const { return (move & MOVE_PROMOTE_MASK) != 0; }
-	inline Piece subject() const { return static_cast<Piece>((move & MOVE_SUBJECT_MASK) >> MOVE_SUBJECT_SHIFT); }
-	inline Piece capture() const { return static_cast<Piece>((move & MOVE_CAPTURE_MASK) >> MOVE_CAPTURE_SHIFT); }
+	inline Square to() const { return Square(move); }
 
 	//演算子オーバーロード
 	bool operator==(const Move &rhs) const { return (move == rhs.move); }
@@ -75,22 +32,12 @@ public:
 	bool operator<(const Move &rhs) const { return (score < rhs.score); }
 	bool operator>(const Move &rhs) const { return (score > rhs.score); }
 
-    inline bool isQuiet() const {
-        return (capture() == EMPTY);
-    }
-
     int32_t toLabel() const;
 
 	//探索時にSeacherクラスから気軽にアクセスできるようpublicにおいてるけど
-	int move;
+	int32_t move;
 	Score score;
 };
-
-//駒を打つ手
-inline Move dropMove(Square to, Piece p) { return Move(to, WALL00, true, false, p, EMPTY); }
-
-//駒を動かす手を引数として、それの成った動きを返す
-inline Move promotiveMove(Move non_promotive_move) { return Move(non_promotive_move.to(), non_promotive_move.from(), false, true, non_promotive_move.subject(), non_promotive_move.capture()); }
 
 //比較用
 const Move NULL_MOVE(0);
