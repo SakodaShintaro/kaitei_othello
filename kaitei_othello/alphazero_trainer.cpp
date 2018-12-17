fc1fce5a6eca1fb757a27e477d10040aac2db93c
SakodaShintaro <rgbygscrsedppbwg@gmail.com>
2018-12-17 14:34:12 +0900

    TD法を復活.random_moveを最大まで上げることが重要なのでは？

--- kaitei_othello/alphazero_trainer.cpp
+++ kaitei_othello/alphazero_trainer.cpp
@@ -342,13 +342,11 @@ void AlphaZeroTrainer::pushOneGame(Game& game) {
         win_rate_for_black = LAMBDA * win_rate_for_black + (1.0 - LAMBDA) * curr_win_rate;
 
 #ifdef USE_CATEGORICAL
-        ////手番から見た分布を得る
-        //auto teacher_dist = onehotDist(pos.color() == BLACK ? win_rate_for_black : 1.0 - win_rate_for_black);
+        //手番から見た分布を得る
+        auto teacher_dist = onehotDist(pos.color() == BLACK ? win_rate_for_black : 1.0 - win_rate_for_black);
 
-        ////teacherにコピーする
-        //for (int32_t j = 0; j < BIN_SIZE; j++) {
-        //    game.teachers[i][POLICY_DIM + j] = teacher_dist[j];
-        //}
+        //teacherにコピーする
+        std::copy(teacher_dist.begin(), teacher_dist.end(), game.teachers[i].begin() + POLICY_DIM);
 #else
         //teacherにコピーする
         game.teachers[i][POLICY_DIM] = (CalcType)(pos.color() == BLACK ? win_rate_for_black : 1.0 - win_rate_for_black);
